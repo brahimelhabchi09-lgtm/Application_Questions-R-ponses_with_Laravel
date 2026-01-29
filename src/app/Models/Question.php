3d592fd7a03d240f80e6bee24f10857d4fef07df
brahimelhabchi09-lgtm <brahimelhabchi09@gmail.com>
2026-01-29 15:30:21 +0100

    feat: create Question model for managing questions

---
+++ src/app/Models/Question.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'content',
+        'user_id',
+    ];
+
+    /* ================= Relations ================= */
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function responses()
+    {
+        return $this->hasMany(Reponse::class);
+    }
+
+    public function favorites()
+    {
+        return $this->hasMany(Favorite::class);
+    }
+}
