c7815be4eb8b86f2377e929e2ec78f3ef2f7dd5f
brahimelhabchi09-lgtm <brahimelhabchi09@gmail.com>
2026-01-29 15:30:04 +0100

    feat: create Admin model for managing admin users

---
+++ src/app/Models/Admin.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Admin extends Model
+{
+    protected $table = 'admins';
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+     /* ================= Relations ================= */
+        
+}
