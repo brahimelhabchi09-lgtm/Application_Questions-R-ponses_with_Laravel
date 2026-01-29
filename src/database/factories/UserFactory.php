247b72b2700806ab073f1c33b398c250f7ced034
brahimelhabchi09-lgtm <brahimelhabchi09@gmail.com>
2026-01-29 15:32:55 +0100

    feat: add UserFactory for generating random user data

---
+++ src/database/factories/UserFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class UserFactory extends Factory
+{
+    /**
+     * The current password being used by the factory.
+     */
+    protected static ?string $password;
+    
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'password' => static::$password ??= Hash::make('password'),
+        ];
+    }
+
+    /**
+     * Indicate that the model's email address should be unverified.
+     */
+}
