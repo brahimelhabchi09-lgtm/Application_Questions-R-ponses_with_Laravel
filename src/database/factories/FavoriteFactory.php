1c34518dfa7f4904e788931e6d3462f87ba55822
brahimelhabchi09-lgtm <brahimelhabchi09@gmail.com>
2026-01-29 15:32:32 +0100

    feat: add FavoriteFactory for generating random user-question relationships

---
+++ src/database/factories/FavoriteFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Question;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class FavoriteFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        
+        return [
+            'user_id' => User::inRandomOrder()->value('id'),
+            'question_id' => Question::inRandomOrder()->value('id'),
+        ];
+    }
+}
