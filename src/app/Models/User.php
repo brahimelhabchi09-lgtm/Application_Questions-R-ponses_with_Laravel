278b31a5aca5b56edf6528b32fd3916b3543972a
brahimelhabchi09-lgtm <brahimelhabchi09@gmail.com>
2026-01-29 15:31:00 +0100

    feat: create User model for managing users

---
+++ src/app/Models/User.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'role', // admin | user
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+
+    /* ================= Relations ================= */
+
+    public function questions()
+    {
+        return $this->hasMany(Question::class);
+    }
+
+    public function responses()
+    {
+        return $this->hasMany(Reponse::class);
+    }
+
+    public function favorites()
+    {
+        return $this->hasMany(Favorite::class);
+    }
+}
